7f7002012936a43f64d16d081ee8a0ee0c0b8e50
Andrew Phillips <theasp@gmail.com>
2021-08-21 18:34:52 +0000

    Add ibdiagnet-lst preset

--- src/parseit/presets.cljs
+++ src/parseit/presets.cljs
@@ -1,7 +1,17 @@
 (ns parseit.presets
   (:require
    [parseit.errors :as errors]
-   [parseit.transforms :as transforms]
+   [parseit.transforms
+    :as transforms
+    :refer [wrap-type
+            transform-map
+            transform-list
+            transform-merge
+            transform-map-kv
+            transform-first
+            transform-integer
+            transform-hex-to-int
+            transform-int-to-hex]]
    [instaparse.core :as insta :refer-macros [defparser]]
    [clojure.string :as str]))
 
@@ -17,16 +27,16 @@
   {:desc      "NSS passwd(5), i.e. /etc/passwd"
    :parser    passwd-parser
    :transform (merge transforms/standard
-                     {:user   transforms/transform-map
-                      :passwd transforms/transform-list})})
+                     {:user   transform-map
+                      :passwd transform-list})})
 
 (defparser group-parser "group.ebnf")
 (def group-preset
   {:desc      "NSS group(5), i.e. /etc/group"
    :parser    group-parser
    :transform (merge transforms/standard
-                     {:group  transforms/transform-map
-                      :groups transforms/transform-list
+                     {:group  transform-map
+                      :groups transform-list
                       :user   str})})
 
 (defparser hosts-parser "hosts.ebnf")
@@ -34,8 +44,8 @@
   {:desc      "NSS hosts(5), i.e. /etc/hosts"
    :parser    hosts-parser
    :transform (merge transforms/standard
-                     {:hosts            transforms/transform-list
-                      :host             transforms/transform-map
+                     {:hosts            transform-list
+                      :host             transform-map
                       :hostname_segment str
                       :hostname         str
                       :ipv4             str
@@ -46,36 +56,57 @@
   {:desc      "High Performance Linpack benchmark results"
    :parser    hpl-parser
    :transform (merge transforms/standard
-                     {:hpl            transforms/transform-map
-                      :authors        (transforms/wrap-type transforms/transform-list :authors)
-                      :header         (transforms/wrap-type transforms/transform-map :header)
-                      :result_timings (transforms/wrap-type transforms/transform-merge :result_timings)
-                      :results        (transforms/wrap-type transforms/transform-map :results)
-                      :result         transforms/transform-map
+                     {:hpl            transform-map
+                      :authors        (wrap-type transform-list :authors)
+                      :header         (wrap-type transform-map :header)
+                      :result_timings (wrap-type transform-merge :result_timings)
+                      :results        (wrap-type transform-map :results)
+                      :result         transform-map
                       :WORD           str
-                      :settings_body  transforms/transform-merge
-                      :kv_item        transforms/transform-map-kv
-                      :kv_key         transforms/transform-first
-                      :kv_value       transforms/transform-first})})
+                      :settings_body  transform-merge
+                      :kv_item        transform-map-kv
+                      :kv_key         transform-first
+                      :kv_value       transform-first})})
 
 (defparser ibnetdiscover-parser "ibnetdiscover.ebnf")
 (def ibnetdiscover-preset
-  {:desc      "Infiniband ibnetdiscover"
+  {:desc      "Infiniband ibnetdiscover output"
    :parser    ibnetdiscover-parser
    :transform (merge transforms/standard
-                     {:ibnetdiscover transforms/transform-map
-                      :header        (transforms/wrap-type transforms/transform-merge :header)
-                      :initiated     transforms/transform-map
-                      :generated     transforms/transform-map
-                      :node          transforms/transform-map
-                      :port          transforms/transform-map})})
+                     {:ibnetdiscover transform-map
+                      :header        (wrap-type transform-merge :header)
+                      :initiated     transform-map
+                      :generated     transform-map
+                      :node          transform-map
+                      :port          transform-map})})
+
+(defparser ibdiagnet-lst-parser "ibdiagnet-lst.ebnf")
+(def ibdiagnet-lst-preset
+  {:desc      "Infiband ibdiagnet.lst"
+   :parser    ibdiagnet-lst-parser
+   :transform (merge transforms/standard
+                     {:port          (wrap-type transform-hex-to-int :port)
+                      :lid           (wrap-type transform-hex-to-int :lid)
+                      :ports         (wrap-type transform-hex-to-int :ports)
+                      :versions      transform-merge
+                      :version_item  transform-map-kv
+                      :version_name  str
+                      :version_value str
+                      :header        (wrap-type transform-map :header)
+                      :conn          (wrap-type transform-map :conn)
+                      :links         (wrap-type transform-list :links)
+                      :link          transform-map
+                      :node          transform-map
+                      :ibdiagnet_lst transform-map})})
+
 (def presets
   {:csv           csv-preset
    :passwd        passwd-preset
    :group         group-preset
    :hosts         hosts-preset
    :hpl           hpl-preset
-   :ibnetdiscover ibnetdiscover-preset})
+   :ibnetdiscover ibnetdiscover-preset
+   :ibdiagnet-lst ibdiagnet-lst-preset})
 
 (defn load-preset [{:keys [options] :as state}]
   (if-let [preset-name (-> state :options :preset)]
